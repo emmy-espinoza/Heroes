acd40b8322aa1e2e35e40a39797867d3974d4163
emmy-espinoza <emilceespinoza22@gmail.com>
2021-07-05 18:39:48 -0300

    arreglo css

--- app.js
+++ app.js
@@ -12,21 +12,30 @@ app.listen(port, () => {
 app.get ('/', (req, res)=>{
   res.sendFile(path.join(__dirname, '/views/home.html'))
 });
+
 app.get('/benners-lee',(req, res)=>{
     res.sendFile(path.join(__dirname, '/views/benners-lee.html'))
-})
-app.get('/clarke',(req, res)=>{
-  res.sendFile(path.join(__dirname, '/views/clarke.html'))
-})
+});
+
+app.get('/babbage',(req, res)=>{
+  res.sendFile(path.join(__dirname, '/views/babbage.html'))
+});
+
+app.get('/lovelace',(req, res)=>{
+  res.sendFile(path.join(__dirname, '/views/lovelace.html'))
+});
+
 app.get('/hamilton',(req, res)=>{
   res.sendFile(path.join(__dirname, '/views/hamilton.html'))
-})
+});
 app.get('/hopper',(req, res)=>{
   res.sendFile(path.join(__dirname, '/views/hopper.html'))
-})
-app.get('/lovelance',(req, res)=>{
-  res.sendFile(path.join(__dirname, '/views/lovelance'))
-})
+});
+
+app.get('/clarke',(req, res)=>{
+  res.sendFile(path.join(__dirname, '/views/clarke.html'))
+});
+
 app.get('/turing',(req, res)=>{
   res.sendFile(path.join(__dirname, '/views/turing.html'))
-})
+});
